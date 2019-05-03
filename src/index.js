6660c063e35b9ddf305a7e09c296d783f8dc10dc
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2019-05-04 01:07:55 +0900

    Added 7 cliffs

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ const App = () => (
       <a-asset-item id="mountains" src="assets/mountains/mountains.gltf" />
       <a-asset-item id="cubes" src="assets/cubes/cubes.gltf" />
       <a-asset-item id="ruins" src="assets/ruins/ruins.gltf" />
+      <a-asset-item id="cliff" src="assets/cliff/cliff.gltf" />
     </a-assets>
 
     <a-entity light="type: hemisphere; color: #ffffff; groundColor: #ffffff; intensity: 0.5" />
@@ -59,8 +60,17 @@ const App = () => (
 
     <a-sphere color="yellow" radius="0.01" position="0 1 -4"  shadow="cast: true; receive: true" ></a-sphere>
 
-    <a-gltf-model src="#ruins" position="5 0.44 -4" set-gltf-material />
-    <a-gltf-model src="#mountains" scale="0.5 0.5 0.5" set-gltf-material="color: #e2aa73;" />
+    <a-gltf-model src="#ruins" position="5 0.1 -4" set-gltf-material />
+    <a-gltf-model src="#mountains" scale="0.5 0.1 0.5" set-gltf-material="color: #e2aa73;" />
+
+    <a-gltf-model id="cliff1" rotation="0 -139.75185468374448 0" src="#cliff" position="76.8745 -5.17808 -101.02159" scale="1.01696 1.77721 1.6834" set-gltf-material="color: #e2aa73"></a-gltf-model>
+    <a-gltf-model id="cliff2" rotation="0 10.34188820211136 0" src="#cliff" position="155.357 -0.93098 -35.56886" scale="1.5073 0.96664 2.1263" set-gltf-material="color: #e2aa73"></a-gltf-model>
+    <a-gltf-model id="cliff3" rotation="179.9998479605043 62.488496010352975 -179.9998479605043" src="#cliff" position="11.33276 -1.21171 -203.52688" scale="0.63449 1.22352 1.05029" set-gltf-material="color: #e2aa73"></a-gltf-model>
+    <a-gltf-model id="cliff4" rotation="0 -70.97113616726482 0" src="#cliff" position="-163.13671 -1.82796 -61.90092" scale="2.28821 1.39396 3.35915" set-gltf-material="color: #e2aa73"></a-gltf-model>
+    <a-gltf-model id="cliff5" rotation="179.99927500270914 -85.17304103517253 179.99927500270914" src="#cliff" position="-86.8801 -1.82798 177.55686" scale="1.73741 1.19296 1.8559" set-gltf-material="color: #e2aa73"></a-gltf-model>
+    <a-gltf-model id="cliff6" rotation="0 -70.97113616726482" src="#cliff" position="84.8113 -1.82796 217.52888" scale="2.28821 1.39396 3.35915" set-gltf-material="color: #e2aa73"></a-gltf-model>
+    <a-gltf-model id="cliff7" rotation="0 19.987059725343638 0" src="#cliff" position="-437.62339 -1.82796 128.33767" scale="1.94443 1.19725 1.94443" set-gltf-material="color: #e2aa73"></a-gltf-model>
+
     {/* <a-gltf-model src="#cubes" set-gltf-material="color: #a98457;" /> */}
   </a-scene>
 );
